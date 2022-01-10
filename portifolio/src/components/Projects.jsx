464f95480c9b4b8cbc6a499c4b27150a707b01c7
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-01-09 21:44:11 -0300

    feat: add curriculum

--- portifolio/src/components/Projects.jsx
+++ portifolio/src/components/Projects.jsx
@@ -13,11 +13,21 @@ export default function Projects() {
         <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
         <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
         </a>
-        <button className="project-btn"> <img className="project-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIykYGfyU8ZSd23m0wdcUF2vEzoBPWZyHJA&usqp=CAU" alt="project image" />  CSS </button>
-        <button className="project-btn"> <img className="project-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIykYGfyU8ZSd23m0wdcUF2vEzoBPWZyHJA&usqp=CAU" alt="project image" />  JavaScript </button>
-        <button className="project-btn"> <img className="project-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIykYGfyU8ZSd23m0wdcUF2vEzoBPWZyHJA&usqp=CAU" alt="project image" />  React </button>
-        <button className="project-btn"> <img className="project-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIykYGfyU8ZSd23m0wdcUF2vEzoBPWZyHJA&usqp=CAU" alt="project image" />  Redux </button>
-        <button className="project-btn"> <img className="project-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIykYGfyU8ZSd23m0wdcUF2vEzoBPWZyHJA&usqp=CAU" alt="project image" />  Redux </button>
+        <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
+        <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
+        </a>
+        <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
+        <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
+        </a>
+        <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
+        <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
+        </a>
+        <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
+        <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
+        </a>
+        <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
+        <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
+        </a>
        </div>
       </div>
     </div>
