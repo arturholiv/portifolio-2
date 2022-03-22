24abd13d3029c284577891885d8bf7f35ecae51d
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-03-22 19:51:50 -0300

    feat: add todolist

--- portifolio/src/components/Projects.jsx
+++ portifolio/src/components/Projects.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import spotitrybeprint from '../images/spotitrybeprint.png'
-import trybewalletprint from '../images/trybewalletprint.png'
-import starwarsplanets from '../images/starwarsplanets.png'
+import { spotitrybeprint, trybewalletprint, starwarsplanets, todolistprint } from '../images'
 import { MdOutlineWeb } from 'react-icons/md'
 
 export default function Projects() {
@@ -12,19 +10,19 @@ export default function Projects() {
       <div className="my-projects">
        <h3>Alguns projetos</h3>
        <div className="projects-buttons">
-        <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" className="project-btn-a project-btn-a" >
-        <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
+        <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" className="project-btn-a project-btn-a" rel='noreferrer'>
+        <button className="project-btn"> <img className="project-image" alt="spoti print" src={ spotitrybeprint } /> SpotiTrybe </button>
         </a>
-        <a href="https://arturholiv.github.io/appdespesas/" target="_blank"  className="project-bt-a project-btn-b">
-        <button className="project-btn"> <img className="project-image" src={ trybewalletprint } /> App Carteira de Câmbio </button>
+        <a href="https://arturholiv.github.io/appdespesas/" target="_blank"  className="project-bt-a project-btn-b" rel='noreferrer'>
+        <button className="project-btn"> <img className="project-image" alt="wallet print" src={ trybewalletprint } /> App Carteira de Câmbio </button>
         </a>
-        <a href="https://arturholiv.github.io/StarWarsPlanetSearch/" target="_blank" >
-        <button className="project-btn"> <img className="project-image" src={ starwarsplanets } /> StarWars Planets Data </button>
+        <a href="https://arturholiv.github.io/StarWarsPlanetSearch/" target="_blank" rel='noreferrer'>
+        <button className="project-btn"> <img className="project-image" alt="starwars print" src={ starwarsplanets } /> StarWars Planets Data </button>
         </a>
-        {/* <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
-        <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
+         <a href="https://arturholiv.github.io/todolist/" target="_blank" rel='noreferrer'>
+        <button className="project-btn"> <img className="project-image" alt="toodlist print" src={ todolistprint } /> To Do List </button>
         </a>
-        <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
+        {/*<a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
         <button className="project-btn"> <img className="project-image" src={ spotitrybeprint } /> SpotiTrybe </button>
         </a>
         <a href="https://arturholiv.github.io/spotitrybe/" target="_blank" >
